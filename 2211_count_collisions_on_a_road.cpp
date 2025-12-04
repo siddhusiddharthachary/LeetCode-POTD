ceeb515f205d6e91766522ac6d916b698ea13b46
rsaisiddhu11 <151196424+siddhusiddharthachary@users.noreply.github.com>
2025-12-04 07:20:37 +0530

    Create 2211_count_collisions_on_a_road.cpp

---
+++ 2211_count_collisions_on_a_road.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int countCollisions(string s) {
+        int n = s.size();
+        int i = 0, j = n - 1;
+
+        while (i < n && s[i] == 'L') i++;
+
+        while (j >= 0 && s[j] == 'R') j--;
+
+        int collisions = 0;
+        for (int k = i; k <= j; k++) {
+            if (s[k] == 'L' || s[k] == 'R')
+                collisions++;
+        }
+        return collisions;
+    }
+};
